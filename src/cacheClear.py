f3134398e87e0bd34c332407d075bb9a196c721b
Saksham Badoni <sbadoni.cs@doonuniversity.ac.in>
2023-11-16 19:14:05 +0530

    Updated cacheClear.py

--- src/cacheClear.py
+++ src/cacheClear.py
@@ -1,21 +1,7 @@
 import os
 
 def cacheClear():
-
-    directory_path = "/home/pc-02/drakvuf"
-    drakvuf_file_name = "processed_exec.txt"
-    drakvuf_file_path = os.path.join(directory_path, drakvuf_file_name)
-
-    # Check if the file exists
-    if os.path.exists(drakvuf_file_path):
-        # Open the file in write mode to empty its content
-        with open(drakvuf_file_path, "w") as file:
-            file.truncate(0)
-        print(f"Content of {drakvuf_file_name} has been emptied.")
-    else:
-        print(f"{drakvuf_file_name} does not exist in {directory_path}.")
-
-
+    
     def clear_folder(folder_path):
         try:
             for filename in os.listdir(folder_path):
@@ -31,7 +17,6 @@ def cacheClear():
             clear_folder(folder_path)
 
     folders_to_clear = [
-        "/var/LearnStreamlit/cacheClear.py",
         # Add more folder paths here
     ]
 
